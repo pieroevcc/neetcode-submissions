1233e06e08c478d2e7e7c17cc608af610126184e
Piero Evangelista <pierevco@gmail.com>
2026-06-20 20:51:40 -0400

    Add: rotting-fruit - submission-5

---
+++ Data Structures & Algorithms/rotting-fruit/submission-5.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<List<Integer>> q = new LinkedList<>();
+        int fresh = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 2){
+                    q.add(Arrays.asList(i, j));
+                }
+                if (grid[i][j] == 1) fresh++;
+            }
+        }
+        int count = 0;
+        int[][] dirs = {{0,-1}, {-1, 0}, {1, 0}, {0, 1}};
+        while(!q.isEmpty()){
+            int size = q.size();
+            boolean t= false;
+            for (int k = 0; k < size; k++){
+                List<Integer> curr = q.poll();
+                int i = curr.get(0);
+                int j = curr.get(1);
+                for(int[] dir : dirs){
+                    int nextI = i + dir[0];
+                    int nextJ = j + dir[1];
+                    if (nextI >= 0 && nextI < grid.length && nextJ >= 0 && nextJ < grid[0].length && grid[nextI][nextJ] == 1){
+                        q.add(Arrays.asList(nextI, nextJ));
+                        grid[nextI][nextJ] = 2;
+                        fresh--;
+                        t = true;
+                    }
+                }
+            }
+            if (t) count++;
+        }
+        if (fresh != 0) return -1;
+        return count;
+    }
+}
