54b93588a4373a09e80c31a504eae920e77ce16c
Piero Evangelista <pierevco@gmail.com>
2026-06-22 13:27:55 -0400

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    //check the min of the next cost 
+    public int minCostClimbingStairs(int[] cost) {
+        int[] cache = new int[cost.length];
+        return Math.min(rec(0, cost, cache), rec(1, cost, cache));
+    }
+
+    public int rec(int i, int[] cost, int[] cache){
+        if (i >= cost.length) return 0;
+        if(cache[i] > 0) return cache[i];
+        cache[i] += Math.min(rec(i+1, cost, cache), rec(i+2, cost, cache));
+        cache[i] += cost[i];
+        return cache[i];
+    }
+}
