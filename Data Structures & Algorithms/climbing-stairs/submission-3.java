401e6c4aeb1f6caab5dc34ec91a35b1648067c13
Piero Evangelista <pierevco@gmail.com>
2026-06-22 00:05:34 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        return rec(0, n , new int[n]);
+    }
+
+    public int rec(int i, int n, int[] cache){
+        if(i > n) return 0;
+        if(i == n) return 1;
+        if(cache[i] > 0) return cache[i];
+        cache[i] = rec(i+1, n , cache) + rec(i+2, n ,cache);
+        return cache[i];
+    }
+}
