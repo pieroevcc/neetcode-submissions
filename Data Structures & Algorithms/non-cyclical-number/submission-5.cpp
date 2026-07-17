0d843842fafc2814610f46b3d320a72b85f4f1eb
Piero Evangelista <pierevco@gmail.com>
2026-07-16 20:28:40 -0400

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    std::unordered_set<int> v;
+    bool isHappy(int n) {
+        if (v.contains(n)) return false;
+        std::string s = std::to_string(n);
+        const char* c = s.c_str();
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++){
+            sum += (c[i] - '0') * (c[i] - '0');
+        }
+        v.emplace(n);
+        if (sum != 1){
+            return isHappy(sum);
+        }else{
+            return true;
+        }
+    }
+};
