e66a027b336a131cc5474f2841c8d02d461a62f6
Piero Evangelista <pierevco@gmail.com>
2026-07-20 12:49:23 -0400

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while(x !=0){
+            if (res > INT_MAX/10 || res < INT_MIN/10) return 0;
+            int digit = x%10;
+            res = res * 10 + digit;
+            x /= 10;
+        }
+        return res;
+    }
+};
