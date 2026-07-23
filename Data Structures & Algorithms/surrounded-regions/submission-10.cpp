26e771d3f4585255bdb4ccad903d16424d171f78
Piero Evangelista <pierevco@gmail.com>
2026-07-22 21:59:14 -0400

    Add: surrounded-regions - submission-10

---
+++ Data Structures & Algorithms/surrounded-regions/submission-10.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        if (m <= 2) return;
+        int n = board[0].size();
+        if (n <= 2) return;
+
+        for (int i = 0; i < n; i++){
+            if (board[0][i] == 'O') dfs(board, 0, i);
+            if (board[m-1][i] == 'O' ) dfs(board, m-1, i); 
+        }
+
+        for (int i = 0; i < m; i++){
+            if (board[i][0] == 'O') dfs(board, i, 0); 
+            if (board[i][n-1] == 'O') dfs(board, i, n-1);
+        }
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+                else if (board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        return;
+    }
+
+    void dfs(vector<vector<char>>&board, int i, int j){
+        if (i > board.size()-1 || j > board[0].size()-1 || i < 0 || j < 0) return;
+        if (board[i][j] != 'O') return;
+        if(board[i][j] == 'O') board[i][j] = '#';
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+        return;
+    }
+};
